79403ff40ce9f4c019a41f0ab419e1a8ec359e30
ivan Ivanov <pp1yshka@mail.ru>
2022-12-03 15:55:44 +0300

    add new page (word) and link | add migration

--- mywebsite/base/views.py
+++ mywebsite/base/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'base/home.html')
+
+def word(request):
+	return render(request, 'base/word.html')
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base',
+    'base', #link on page (home.html and word.html)
 ]
 
 MIDDLEWARE = [
